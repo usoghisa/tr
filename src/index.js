63988ad17f7b15713b61affcbffce62f9918ea24
usoghisa <usoghisa@users.noreply.github.com>
2019-12-02 14:17:02 +0000

    ww
    111

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>2Hello CodeSandbox</h1>
+      <h1>2Hellorrr CodeSandbox</h1>
       <h2>Start editing to see some magic happen!333</h2>
     </div>
   );
